23821d9b52d2ebd761a7f56ab1bdb48aef6960e2
hasanisu <hasanjimam@gmail.com>
2023-11-20 10:53:09 +0900

    first commit

---
+++ src/app/app.ts
@@ -0,0 +1,97 @@
+import exp from 'constants';
+import express, { NextFunction, Request, Response } from 'express'
+import { execArgv } from 'process';
+const app = express()
+
+
+//parsers
+app.use(express.json());
+app.use(express.text());
+
+
+// Router 
+const userRouter = express.Router();
+const courseRouter = express.Router();
+
+
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/courses', courseRouter);
+
+userRouter.post(
+  '/create-user',
+  (req: Request, res: Response) => {
+
+    const user = req.body;
+    console.log(user)
+    res.json({
+      success:true,
+      message: 'User is Created successfully',
+      data: user
+    })
+  })
+
+
+
+  courseRouter.post('/create-course', (req:Request, res:Response)=>{
+    const course = req.body;
+    console.log(course);
+    res.json({
+      success: true,
+      message:'User is created succesfully',
+      data: course
+    })
+  })
+
+
+
+
+
+//middleWare
+const logger = (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.url, req.method, req.hostname);
+  next();
+}
+
+app.get('/', logger, async(req: Request, res: Response, next:NextFunction) => {
+  try {
+    res.send(something)
+  } catch (error) {
+    console.log(error)
+
+    next(error)
+    // res.status(400).json({
+     
+    //   success: false,
+    //   message: 'failed to get data'
+    // })
+  }
+  
+})
+
+
+app.post('/', logger, (req: Request, res: Response) => {
+  console.log(req.body);
+  res.send('got dat')
+})
+
+
+
+
+app.all("*",(req: Request, res:Response)=>{
+  res.status(400).json({
+    success: false,
+    message: 'Route is not found'
+  })
+})
+
+//Golbal Error handler
+app.use((error:any, req:Request, res:Response, next:NextFunction)=>{
+  if(error){
+    res.status(400).json({
+      success:false,
+      message:'Something went wrong'
+    })
+  }
+})
+
+export default app;
